3efa8f0d437fb5e0db3444f3813f4aa45a53510c
Tyler Hall <hal12062@byui.edu>
2016-01-12 15:26:47 -0700

    minor changes

--- BlocBrowser/ViewController.m
+++ BlocBrowser/ViewController.m
@@ -42,7 +42,7 @@
     self.textField.returnKeyType = UIReturnKeyDone;
     self.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
     self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
-    self.textField.placeholder = NSLocalizedString(@"Search", @"Placeholder text for web browser URL field");
+    self.textField.placeholder = NSLocalizedString(@"Search", @"Placeholder text for web browser text field");
     self.textField.backgroundColor = [UIColor colorWithWhite:220/255.0f alpha:1];
     self.textField.delegate = self;
     
