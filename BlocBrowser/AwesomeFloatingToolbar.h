7ec63f36c0ddaa2a2c61164e2276cf85f1d585bd
Tyler Hall <hal12062@byui.edu>
2016-01-26 18:06:07 -0700

    checkpoint 26 complete

--- BlocBrowser/AwesomeFloatingToolbar.h
+++ BlocBrowser/AwesomeFloatingToolbar.h
@@ -18,14 +18,25 @@
 
 - (void) floatingToolbar:(AwesomeFloatingToolbar *)toolbar didSelectButtonWithTitle:(NSString *)title;
 
+- (void) floatingToolbar:(AwesomeFloatingToolbar *)toolbar didTryToPanWithOffset:(CGPoint)offset;
+
 @end
 
 @interface AwesomeFloatingToolbar : UIView
 
 - (instancetype) initWithFourTitles:(NSArray *)titles;
 
+
+
 - (void) setEnabled:(BOOL)enabled forButtonWithTitle:(NSString *)title;
 
+
+
+
 @property (nonatomic, weak) id <AwesomeFloatingToolbarDelegate> delegate;
+
+
+
+
 
 @end
